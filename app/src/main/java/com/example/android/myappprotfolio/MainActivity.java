42d660212b411e8999da573509349c6c8a60d073
Vincent0-0 <360658186.qq.com>
2016-08-11 00:37:29 +0800

    optimized

--- app/src/main/java/com/example/android/myappprotfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappprotfolio/MainActivity.java
@@ -17,39 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void showToastPopularMovie(View view) {
+    public void showToast(View view) {
+        Button button = (Button) view;
+        String appName = button.getText().toString();
         Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my popular movies app!", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void showToastStockHawk(View view) {
-        Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my stock hawk app!", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void showToastBuildItBigger(View view) {
-        Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my build it bigger app!", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void showToastMakeYourAppMaterial(View view) {
-        Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my make your app material app!", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void showToastGoUbiquitous(View view) {
-        Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my go ubiquitous app!", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void showToastCapstone(View view) {
-        Toast toast = Toast.makeText(
-                getApplicationContext(), "This button will launch my capstone app!", Toast.LENGTH_SHORT);
+                getApplicationContext(), "This button will launch my " + appName +" app!", Toast.LENGTH_SHORT);
         toast.show();
     }
 }
